4c5100fd115ca886e19da59fd3603fd58f415d13
St-Preux <ty@wc-dhcp96d085.employee-secure.wireless.fsu.edu>
2019-01-31 14:19:59 -0500

    Added CrazyRandomSword implementation
    CrazyRandomSword.cpp has been added to implement the function of the CrazyRandomSword class.

---
+++ CrazyRandomSword.cpp
@@ -0,0 +1,10 @@
+#include "CrazyRandomSword.h"
+#include<ctime>
+
+double CrazyRandomSword::hit(double armor) {
+    double damage = hitPoints - rand()%(int)(armor*.3)+2;
+    if(damage < 0){
+        return 0;
+    }
+    return damage;
+}
