67df441adfe0d3b7e14cca22f9a7f35f6b5e2fb2
St-Preux <ty@wc-dhcp96d085.employee-secure.wireless.fsu.edu>
2019-01-31 12:42:45 -0500

    Added SimpleHammer header
    SimpleHammer.h has been added to declare a class of SimpleHammer with a function.
    answers.txt has been updated with answers for some questions.

---
+++ SimpleHammer.h
@@ -0,0 +1,23 @@
+#include <string>
+#include "Weapon.h"
+
+#ifndef SIMPLEHAMMER_H
+#define SIMPLEHAMMER_H
+
+/**
+ * Defines the behavior of a common spear (hitpoint = 40, ignores 20% of 
+ * armor points)
+ */
+class SimpleHammer : public Weapon {
+public:
+
+    SimpleHammer() : Weapon("Simple hammer", 25.0) { //Calls Weapon(name, hitpoints) constructor with values Common Spear and 40.0
+    }
+
+    virtual ~SimpleHammer() {};
+
+    virtual double hit(double armor);
+
+};
+
+#endif /* SIMPLEHAMMER_H */
