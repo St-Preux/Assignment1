f816a16174de3d9f2021b6069cc105c14f894998
St-Preux <ty@wc-dhcp96d085.employee-secure.wireless.fsu.edu>
2019-01-31 15:27:17 -0500

    Updated WeaponFactory implementation
    WeaponFactory.cpp has been updated to accommodate the new created weapon classes.

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -9,6 +9,9 @@
 #include "WeaponFactory.h"
 #include "CommonSword.h" 
 #include "CommonSpear.h"
+#include "CrazyRandomSword.h"
+#include "EbonySword.h"
+#include "SimpleHammer.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
@@ -26,6 +29,18 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
     
     if (name.compare("spear") == 0) {
         return new CommonSpear();
+    }
+
+    if (name.compare("random sword") == 0) {
+        return new CrazyRandomSword();
+    }
+    
+    if (name.compare("ebony sword") == 0) {
+        return new EbonySword();
+    }
+    
+    if (name.compare("hammer") == 0) {
+        return new SimpleHammer();
     }
 
     throw "Invalid weapon";
